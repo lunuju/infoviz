6752a6c30f91332f3d190f0446a98bfb7c9f95b1
iTitou <moiandme@gmail.com>
2016-03-23 22:01:21 +0100

    [enh] Process day by day

--- extract_trajects_time.py
+++ extract_trajects_time.py
@@ -1,6 +1,7 @@
 import psycopg2 as pg
 import traceback
 from collections import defaultdict
+from datetime import timedelta
 
 STIB_LINES = [
     1, 12, 13, 14, 15, 17, 19, 2, 20, 21, 22, 25, 27, 28, 29, 3, 32, 34, 36,
@@ -35,17 +36,25 @@ def reduce_traject(traject):
     return ret
 
 
-def get_trajects(line, direction):
+def get_trajects(line, direction, from_time, to_time):
     assert direction in [1, 2]
     query = """SELECT * FROM heading
-               WHERE line='%(line)s' AND way=%(direction)s
+               WHERE line='%(line)s' AND
+                     way=%(direction)s AND
+                     timestamp < %(to_time)s AND
+                     timestamp > %(from_time)s
                ORDER BY id;"""
 
     conn = pg.connect(database="delay")
     conn.autocommit = True
 
     cur = conn.cursor()
-    cur.execute(query, {'line': line, 'direction': direction})
+    cur.execute(query, {
+        'line': line,
+        'direction': direction,
+        'from_time': from_time,
+        'to_time': to_time
+    })
     data = cur.fetchall()
 
     aligned = []
@@ -86,32 +95,51 @@ def get_trajects(line, direction):
     return list(trajects)
 
 
-def extract_line(line):
+def extract_line(line, start_time, end_time):
     found = 0
     conn = pg.connect(database='delay')
     cur = conn.cursor()
-    query = 'INSERT INTO legs (departure,arrival,line,from_stop_id,to_stop_id) VALUES (%s,%s,%s,%s,%s)'
-    for direction in [1, 2]:
-        trajs = get_trajects(line['lineNo'], direction)
-        stops = line[str(direction)]
-        for t in trajs:
-            for (t1, idx1), (t2, idx2) in zip(t[:-1], t[1:]):
-                dt = (t2-t1).total_seconds()
-                if dt > 6*3600:
-                    continue
-                record = [t1, t2, line['lineNo'], stops[idx1], stops[idx2]]
-                cur.execute(query, record)
-                found += 1
+
+    cur.execute("SELECT COUNT(*) FROM legs_building WHERE line=%s", (line['lineNo'],))
+    res = cur.fetchone()
+    if res[0] > 0:
+        print "Line", line['lineNo'], "already processed"
+        return
+
+    query = 'INSERT INTO legs_building (departure,arrival,line,from_stop_id,to_stop_id) VALUES (%s,%s,%s,%s,%s)'
+    for i in range((end_time - start_time).days):
+        t0 = start_time + timedelta(days=i)
+        t1 = start_time + timedelta(days=i+1)
+        for direction in [1, 2]:
+            trajs = get_trajects(line['lineNo'], direction, t0, t1)
+            stops = line[str(direction)]
+            for t in trajs:
+                for (t1, idx1), (t2, idx2) in zip(t[:-1], t[1:]):
+                    dt = (t2-t1).total_seconds()
+                    if dt > 6*3600:
+                        continue
+                    record = [t1, t2, line['lineNo'], stops[idx1], stops[idx2]]
+                    cur.execute(query, record)
+                    found += 1
     conn.commit()
     print "Finished line", line['lineNo'], "with", found, "legs"
 
 
+def get_extrema_dates():
+    conn = pg.connect(database='delay')
+    cur = conn.cursor()
+    cur.execute('SELECT MIN(timestamp), MAX(timestamp) FROM heading;')
+    return cur.fetchone()
+
 if __name__ == "__main__":
     import json
 
+    at_3_hour = lambda d: d.replace(hour=3, minute=0, second=0, microsecond=0)
+    from_time, to_time = map(at_3_hour, get_extrema_dates())
+
     for line in json.load(open('stib-lines.json')):
         try:
-            extract_line(line)
+            extract_line(line, from_time, to_time)
         except:
             print "Error with line", line['lineNo']
             traceback.print_exc()
