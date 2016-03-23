daf653376efef3688e1b14bafc6739c6b18658a8
iTitou <moiandme@gmail.com>
2016-03-23 19:51:29 +0100

    [add] Events menu can set selection !!!

--- app/src/main.js
+++ app/src/main.js
@@ -2,8 +2,23 @@ import $ from 'jquery'
 import App from './App.js'
 
 $(document).ready(() => {
-    new App($('#left'))
-    new App($('#right'))
+    let left = new App($('#left'))
+    let right = new App($('#right'))
+
+    let menu = $('#events-menu')
+    menu.find('li').each((_, li) => {
+        $(li).append('<a class="label label-default set-left" href="#">Left</a>')
+        $(li).append('<a class="label label-default set-right" href="#">Right</a>')
+    })
+
+    menu.find('.set-left').click(evt => {
+        let li = $(evt.target).closest('li')
+        left.setRange(li.attr('data-from'), li.attr('data-to'))
+    })
+    menu.find('.set-right').click(evt => {
+        let li = $(evt.target).closest('li')
+        right.setRange(li.attr('data-from'), li.attr('data-to'))
+    })
 })
 
 window.jQuery = $
