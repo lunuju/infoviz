5113c7859e8e11714f6b9bd091fe4fcc6d196ab0
Pierre Gerard <piegerar@ulb.ac.be>
2016-05-02 17:09:35 +0200

    [ref] toolbar

--- app/src/App.js
+++ app/src/App.js
@@ -8,11 +8,35 @@ export default class App {
         this.right = new MapView($('#right'))
         this.makeEventsMenu()
         this.makeUsualMenu()
-        this.makeSliderMenu()
+        this.makeRightSliderMenu()
+        this.makeLeftSliderMenu()
     }
 
-    makeSliderMenu(){
-        let menu = $('#slider-menu')
+    makeRightSliderMenu(){
+        let menu = $('#slider-menu-right')
+        this.slider = menu.find('.slider').ionRangeSlider({
+            hide_min_max: true,
+            keyboard: true,
+            step: 86400000,
+            grid: true,
+            force_edges: true,
+            min: 1449442800000,
+            max: 1458687600000,
+            from: (1458687600000 + 1449442800000)/2,
+            prettify: num => new Date(num).toLocaleString(),
+            onFinish: evt => {}
+        }).data("ionRangeSlider");
+        menu.find('.set-right').click(evt => {
+            let t = this.slider.options.from
+            this.right.setRange(t2s(t-86400000), t2s(t+86400000), t2s(t-86400000), t2s(t+86400000))
+        })
+        menu.click(evt => {
+            evt.stopPropagation();
+        })
+    }
+
+    makeLeftSliderMenu(){
+        let menu = $('#slider-menu-left')
         this.slider = menu.find('.slider').ionRangeSlider({
             hide_min_max: true,
             keyboard: true,
@@ -29,29 +53,37 @@ export default class App {
             let t = this.slider.options.from
             this.left.setRange(t2s(t-86400000), t2s(t+86400000), t2s(t-86400000), t2s(t+86400000))
         })
-        menu.find('.set-right').click(evt => {
-            let t = this.slider.options.from
-            this.right.setRange(t2s(t-86400000), t2s(t+86400000), t2s(t-86400000), t2s(t+86400000))
-        })
         menu.click(evt => {
             evt.stopPropagation();
         })
     }
+
     makeUsualMenu(){
-        let menu = $('#usual-menu')
-        this.makeOneMenu(menu)
+        let menuRight = $('#usual-menu-right')
+        this.makeMenuRight(menuRight)
+        let menuLeft = $('#usual-menu-left')
+        this.makeMenuLeft(menuLeft)
     }
 
     makeEventsMenu(){
-        let menu = $('#events-menu')
-        this.makeOneMenu(menu)
+        let menuRight = $('#events-menu-right')
+        this.makeMenuRight(menuRight)
+        let menuLeft = $('#events-menu-left')
+        this.makeMenuLeft(menuLeft)
     }
 
-    makeOneMenu(menu){
-        menu.find('a').each((_, li) => {
-            $(li).prepend('<span title="Set to right map" class="label label-warning set-right">Right</span>&nbsp;')
-            $(li).prepend('<span title="Set to left map" class="label label-info set-left">Left</span>&nbsp;')
+    makeMenuRight(menu){
+        menu.find('.set-left').click(evt => {
+            let li = $(evt.target).closest('li')
+            this.left.setRange(li.attr('data-min'), li.attr('data-max'), li.attr('data-from'), li.attr('data-to'))
         })
+        menu.find('.set-right').click(evt => {
+            let li = $(evt.target).closest('li')
+            this.right.setRange(li.attr('data-min'), li.attr('data-max'), li.attr('data-from'), li.attr('data-to'))
+        })
+    }
+
+    makeMenuLeft(menu){
         menu.find('.set-left').click(evt => {
             let li = $(evt.target).closest('li')
             this.left.setRange(li.attr('data-min'), li.attr('data-max'), li.attr('data-from'), li.attr('data-to'))
