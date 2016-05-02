11787f6c233eaaf175ce38bed32a86f37b05cac7
iTitou <moiandme@gmail.com>
2016-05-02 18:36:28 +0200

    [add] Cache for initial view + obtain detailled traject times endpoint

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, g
 from flask.ext.cors import CORS
 import psycopg2 as pg
 from psycopg2.extras import RealDictCursor
@@ -16,11 +16,18 @@ def cast(a_dict):
     return a_dict
 
 
-def sql(query, args):
-    conn = pg.connect(database='delay')
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute(query, args)
-    response = make_response(json.dumps(map(cast, cur.fetchall())))
+def sql(query, args, cache=False):
+    if cache and 'cache' in g:
+        as_json = g['cache']
+    else:
+        conn = pg.connect(database='delay')
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(query, args)
+        as_json = json.dumps(map(cast, cur.fetchall()))
+        if cache:
+            g['cache'] = as_json
+
+    response = make_response(as_json)
     response.headers['Content-type'] = 'application/json'
     return response
 
@@ -43,6 +50,23 @@ def api():
     args = {
         'from_time': request.args.get('from_time', '2016-03-01'),
         'to_time': request.args.get('to_time', '2016-03-03')
+    }
+    return sql(query, args, ('from_time' == '2016-03-01 08:00' and 'to_time' == '2016-03-01 10:00'))
+
+
+@app.route("/travel_time")
+def travel_time():
+    query = """
+    SELECT array_agg(extract('epoch' FROM (arrival - departure))) AS travel_times
+    FROM legs
+    WHERE departure >= %(from_time)s AND arrival <= %(to_time)s AND
+          from_stop_id = %(from_stop)s AND to_stop_id = %(to_stop)s;
+    """
+    args = {
+        'from_time': request.args.get('from_time', '2016-03-01'),
+        'to_time': request.args.get('to_time', '2016-03-03'),
+        'from_stop': request.args.get('from_stop', '6304'),
+        'to_stop': request.args.get('to_stop', '6416'),
     }
     return sql(query, args)
 
